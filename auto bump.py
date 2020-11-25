48b31b862ce82947e23768c50c7ec3010b171ab9
Puffy <53114830+Puffy120@users.noreply.github.com>
2020-11-25 11:54:25 +0100

    Add files via upload

---
+++ auto bump.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import os
+import time
+from colorama import Fore, init
+
+os.system("cls")
+
+
+print(f'''{Fore.RESET}
+
+                                  {Fore.WHITE}██████{Fore.CYAN}╗  {Fore.WHITE}██{Fore.CYAN}╗   {Fore.WHITE}██{Fore.CYAN}╗ {Fore.WHITE}███████{Fore.CYAN}╗ {Fore.WHITE}███████{Fore.CYAN}╗ {Fore.WHITE}██{Fore.CYAN}╗   {Fore.WHITE}██{Fore.CYAN}╗                               
+                                  {Fore.WHITE}██{Fore.CYAN}╔══{Fore.WHITE}██{Fore.CYAN}╗ {Fore.WHITE}██{Fore.CYAN}║   {Fore.WHITE}██{Fore.CYAN}║ {Fore.WHITE}██{Fore.CYAN}╔════╝ {Fore.WHITE}██{Fore.CYAN}╔════╝ {Fore.CYAN}╚{Fore.WHITE}██{Fore.CYAN}╗ {Fore.WHITE}██{Fore.CYAN}╔╝
+                                  {Fore.WHITE}██████{Fore.CYAN}╔╝ {Fore.WHITE}██{Fore.CYAN}║   {Fore.WHITE}██{Fore.CYAN}║ {Fore.WHITE}█████{Fore.CYAN}╗   {Fore.WHITE}█████{Fore.CYAN}╗    ╚{Fore.WHITE}████{Fore.CYAN}╔╝
+                                  {Fore.WHITE}██{Fore.CYAN}╔═══╝  {Fore.WHITE}██{Fore.CYAN}║   {Fore.WHITE}██{Fore.CYAN}║ {Fore.WHITE}██{Fore.CYAN}╔══╝   {Fore.WHITE}██{Fore.CYAN}╔══╝     {Fore.CYAN}╚{Fore.WHITE}██{Fore.CYAN}╔╝
+                                  {Fore.WHITE}██{Fore.CYAN}║      ╚{Fore.WHITE}██████{Fore.CYAN}╔╝ {Fore.WHITE}██{Fore.CYAN}║      {Fore.WHITE}██{Fore.CYAN}║         {Fore.WHITE}██{Fore.CYAN}║
+                                  {Fore.CYAN}╚═╝       ╚═════╝  ╚═╝      ╚═╝         ╚═╝
+
+''')
+
+token = input(f"{Fore.CYAN} Token:{Fore.WHITE} ")
+chanID = input(f"{Fore.CYAN} Channel ID:{Fore.WHITE} ")
+
+
+
+chan = f"https://discordapp.com/api/v6/channels/{chanID}/messages"
+
+while True:
+    time.sleep(10)
+    headers = {"authorization": token, "content-type": "application/json"}
+    content = {"content":"!d bump"}
+    requests.post(chan, headers=headers, data=json.dumps(content))
+    print(Fore.GREEN + "[+] Message sent!")
